f5c3b90c01845ba3d227bf3d802f20a37a3b9139
Roger Ellenberger <ellr@wagner.ch>
2022-10-13 16:24:11 +0200

    add agent_based check for aci_dom_rx_pwr_stats

---
+++ agent_based/aci_dom_rx_pwr_stats.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+# This is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+"""
+Check_MK Checks to use with agent_cisco_aci Datasource
+
+Authors:    Roger Ellenberger, roger.ellenberger@wagner.ch
+Version:    0.7
+
+"""
+
+from __future__ import annotations
+from typing import List, NamedTuple, Sequence, Tuple
+import re
+
+from .agent_based_api.v1.type_defs import (
+    CheckResult,
+    DiscoveryResult,
+)
+from .agent_based_api.v1 import (
+    register,
+    check_levels,
+    Result,
+    Service,
+    State,
+)
+
+
+class DomRxPowerStat(NamedTuple):
+    dn: str
+    alert: str
+    status: str
+    hi_alarm: float
+    hi_warn: float
+    lo_alarm: float
+    lo_warn: float
+    value: float
+
+    @property
+    def details(self) -> str:
+        return (
+            f'alert: {self.alert}\n'
+            f'status: {self.status}\n'
+            f'hi_alarm: {self.hi_alarm}\n'
+            f'hi_warn: {self.hi_warn}\n'
+            f'lo_alarm: {self.lo_alarm}\n'
+            f'lo_warn: {self.lo_warn}\n'
+            f'value: {self.value} (precise)'
+        )
+
+    @property
+    def summary(self) -> str:
+        return f'alert: {self.alert}, status: {self.status}'
+
+    @property
+    def state(self) -> State:
+        return State.OK if self.alert == 'none' else State.WARN
+
+    @property
+    def upper_levels(self) -> Tuple:
+        return (self.hi_warn, self.hi_alarm)
+
+    @property
+    def lower_levels(self) -> Tuple:
+        return (self.lo_warn, self.lo_alarm)
+
+    @property
+    def interface(self) -> str:
+        IFACE_REGEX: str = r'\[(?P<iface>eth\d+(/\d+){1,2})\]'
+        iface_regex = re.compile(IFACE_REGEX)
+        matches = iface_regex.search(self.dn)
+        return f'{matches.group("iface")}'
+
+    @staticmethod
+    def from_string_table(line: Sequence) -> DomRxPowerStat:
+        return DomRxPowerStat(
+            dn=line[0],
+            alert=line[1],
+            status=line[2],
+            hi_alarm=float(line[3]),
+            hi_warn=float(line[4]),
+            lo_alarm=float(line[5]),
+            lo_warn=float(line[6]),
+            value=float(line[7]),
+        )
+
+
+def parse_aci_dom_rx_pwr_stats(string_table) -> List[DomRxPowerStat]:
+    """
+    Exmple output:
+        #dn alert status hi_alarm hi_warn lo_alarm lo_warn value
+        topology/pod-1/node-112/sys/phys-[eth1/1]/phys/domstats/rxpower none none 0.999912 0.000000 -13.098040 -12.097149 -2.599533
+        topology/pod-1/node-112/sys/phys-[eth1/11]/phys/domstats/rxpower none none 0.999912 0.000000 -13.098040 -12.097149 -2.910695
+        topology/pod-1/node-112/sys/phys-[eth1/12]/phys/domstats/rxpower none none 0.999912 0.000000 -13.098040 -12.097149 -2.814153
+    """
+    return [
+        DomRxPowerStat.from_string_table(line) for line in string_table
+        if not line[0].startswith('#')
+    ]
+
+
+register.agent_section(
+    name='aci_dom_rx_pwr_stats',
+    parse_function=parse_aci_dom_rx_pwr_stats,
+)
+
+
+def discover_aci_dom_rx_pwr_stats(section: List[DomRxPowerStat]) -> DiscoveryResult:
+    for pwr_stat in section:
+        yield Service(item=pwr_stat.interface)
+
+
+def check_aci_dom_rx_pwr_stats(item: str, section: List[DomRxPowerStat]) -> CheckResult:
+    for stat in section:
+        if item == stat.interface:
+            yield Result(state=stat.state, notice=stat.summary, details=stat.details)
+
+            # Alerting works with dynamic warn/alert levels that are received from ACI
+            yield from check_levels(stat.value,
+                                    levels_upper=stat.upper_levels,
+                                    levels_lower=stat.lower_levels,
+                                    metric_name='dom_rx_power',
+                                    label='value')
+            break
+    else:
+        yield Result(state=State.UNKNOWN, summary='Sorry - item not found')
+
+
+register.check_plugin(
+    name='aci_dom_rx_pwr_stats',
+    service_name='DOM Rx Power %s',
+    discovery_function=discover_aci_dom_rx_pwr_stats,
+    check_function=check_aci_dom_rx_pwr_stats,
+)
---
+++ tests/unit/cmk/base/plugins/agent_based/test_aci_dom_rx_pwr_stats.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# This is free software;  you can redistribute it and/or modify it
+# under the  terms of the  GNU General Public License  as published by
+# the Free Software Foundation in version 2.  check_mk is  distributed
+# in the hope that it will be useful, but WITHOUT ANY WARRANTY;  with-
+# out even the implied warranty of  MERCHANTABILITY  or  FITNESS FOR A
+# PARTICULAR PURPOSE. See the  GNU General Public License for more de-
+# tails. You should have  received  a copy of the  GNU  General Public
+# License along with GNU Make; see the file  COPYING.  If  not,  write
+# to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
+# Boston, MA 02110-1301 USA.
+
+from typing import Tuple, List
+
+import pytest
+
+from cmk.base.plugins.agent_based.agent_based_api.v1 import Result, State, Service, Metric
+from cmk.base.plugins.agent_based.aci_dom_rx_pwr_stats import (
+    parse_aci_dom_rx_pwr_stats,
+    discover_aci_dom_rx_pwr_stats,
+    check_aci_dom_rx_pwr_stats,
+    DomRxPowerStat,
+)
+
+
+SECTION_1: List = [
+    DomRxPowerStat(
+        dn='topology/pod-1/node-101/sys/phys-[eth1/3]/phys/domstats/rxpwer',
+        alert='none',
+        status='none',
+        hi_alarm=5.000031,
+        hi_warn=4.000023,
+        lo_alarm=-13.695722,
+        lo_warn=-11.700533,
+        value=-1.674911,
+    ),
+]
+
+SECTION_2: List = [
+    DomRxPowerStat(
+        dn='topology/pod-1/node-112/sys/phys-[eth1/1]/phys/domstats/rxpower',
+        alert='none',
+        status='none',
+        hi_alarm=0.999912,
+        hi_warn=0.000000,
+        lo_alarm=-13.098040,
+        lo_warn=-12.097149,
+        value=-2.599533,
+    ),
+    DomRxPowerStat(
+        dn='topology/pod-1/node-112/sys/phys-[eth1/11]/phys/domstats/rxpower',
+        alert='warn',
+        status='bla',
+        hi_alarm=0.999912,
+        hi_warn=0.000000,
+        lo_alarm=-13.098040,
+        lo_warn=-12.097149,
+        value=0.910695,
+    ),
+    DomRxPowerStat(
+        dn='topology/pod-1/node-112/sys/phys-[eth11/21/102]/phys/domstats/rxpower',
+        alert='none',
+        status='none',
+        hi_alarm=5.000031,
+        hi_warn=4.000023,
+        lo_alarm=-13.695722,
+        lo_warn=-11.700533,
+        value=-15.648960,
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "string_table, expected_section",
+    [
+        (
+            [],
+            []
+        ),
+        (
+            [
+                ['#dn', 'alert', 'status', 'hi_alarm', 'hi_warn', 'lo_alarm', 'lo_warn', 'value'],
+                ['topology/pod-1/node-101/sys/phys-[eth1/3]/phys/domstats/rxpwer', 'none', 'none', '5.000031', '4.000023', '-13.695722', '-11.700533', '-1.674911'],
+            ],
+            SECTION_1,
+        ),
+        (
+            [
+                # shall also work without a header row
+                ['topology/pod-1/node-112/sys/phys-[eth1/1]/phys/domstats/rxpower', 'none', 'none', '0.999912', '0.000000', '-13.098040', '-12.097149', '-2.599533'],
+                ['topology/pod-1/node-112/sys/phys-[eth1/11]/phys/domstats/rxpower', 'warn', 'bla', '0.999912', '0.000000', '-13.098040', '-12.097149', '0.910695'],
+                ['topology/pod-1/node-112/sys/phys-[eth11/21/102]/phys/domstats/rxpower', 'none', 'none', '5.000031', '4.000023', '-13.695722', '-11.700533', '-15.648960'],
+            ],
+            SECTION_2,
+        ),
+    ],
+)
+def test_parse_aci_dom_rx_pwr_stats(string_table: List[List[str]], expected_section: List[DomRxPowerStat]) -> None:
+    assert parse_aci_dom_rx_pwr_stats(string_table) == expected_section
+
+
+@pytest.mark.parametrize(
+    "section, expected_discovery_result",
+    [
+        (
+            SECTION_1,
+            (
+                Service(item='eth1/3'),
+            ),
+        ),
+        (
+            SECTION_2,
+            (
+                Service(item='eth1/1'),
+                Service(item='eth1/11'),
+                Service(item='eth11/21/102'),
+            ),
+        ),
+    ],
+)
+def test_discover_aci_dom_rx_pwr_stats(section: List[DomRxPowerStat], expected_discovery_result: Tuple) -> None:
+    assert tuple(discover_aci_dom_rx_pwr_stats(section)) == expected_discovery_result
+
+
+@pytest.mark.parametrize(
+    "item, section, expected_check_result",
+    [
+        (
+            '',
+            [],
+            (
+                Result(state=State.UNKNOWN, summary='Sorry - item not found'),
+            ),
+        ),
+        (
+            'eth1/1',
+            SECTION_2,
+            (
+                Result(state=State.OK, notice='alert: none, status: none',
+                       details='alert: none\nstatus: none\nhi_alarm: 0.999912\nhi_warn: 0.0\nlo_alarm: -13.09804\nlo_warn: -12.097149\nvalue: -2.599533 (precise)'),
+                Result(state=State.OK, summary='value: -2.60'),
+                Metric('dom_rx_power', -2.599533, levels=(0.0, 0.999912))
+            )
+        ),
+        (
+            'eth1/11',
+            SECTION_2,
+            (
+                Result(state=State.WARN, notice='alert: warn, status: bla',
+                       details='alert: warn\nstatus: bla\nhi_alarm: 0.999912\nhi_warn: 0.0\nlo_alarm: -13.09804\nlo_warn: -12.097149\nvalue: 0.910695 (precise)'),
+                Result(state=State.WARN, summary='value: 0.91 (warn/crit at 0.00/1.00)'),
+                Metric('dom_rx_power', 0.910695, levels=(0.0, 0.999912))
+            )
+        ),
+        (
+            'eth11/21/102',
+            SECTION_2,
+            (
+                Result(state=State.OK, notice='alert: none, status: none',
+                       details='alert: none\nstatus: none\nhi_alarm: 5.000031\nhi_warn: 4.000023\nlo_alarm: -13.695722\nlo_warn: -11.700533\nvalue: -15.64896 (precise)'),
+                Result(state=State.CRIT, summary='value: -15.65 (warn/crit below -11.70/-13.70)'),
+                Metric('dom_rx_power', -15.64896, levels=(4.000023, 5.000031)),
+            )
+        ),
+    ],
+)
+def test_check_aci_dom_rx_pwr_stats(item: str, section: List[DomRxPowerStat], expected_check_result: Tuple) -> None:
+    assert tuple(check_aci_dom_rx_pwr_stats(item, section)) == expected_check_result
