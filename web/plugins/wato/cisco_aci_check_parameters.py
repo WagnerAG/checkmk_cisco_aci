74397cdfd304dd86c09fb2289e9ba404ad6fbffd
Roger Ellenberger <ellr@wagner.ch>
2022-10-20 18:14:13 +0200

    add wato rulespec to set health levels for fabric and nodes

--- web/plugins/wato/cisco_aci_check_parameters.py
+++ web/plugins/wato/cisco_aci_check_parameters.py
@@ -9,18 +9,20 @@ Author:     Roger Ellenberger <roger.ellenberger@wagner.ch>
 from cmk.gui.i18n import _
 from cmk.gui.plugins.wato.utils import (
     CheckParameterRulespecWithItem,
+    CheckParameterRulespecWithoutItem,
     rulespec_registry,
     RulespecGroupCheckParametersNetworking,
 )
 from cmk.gui.valuespec import (
     Dictionary,
     Float,
+    Integer,
     TextInput,
     Tuple,
 )
 
 
-def _parameter_valuespec_nc_coriant_psus():
+def _parameter_valuespec_aci_l1_phys_if_levels():
     return Dictionary(
         help=_(
             'To obtain the data required for this check, please configure'
@@ -97,7 +99,100 @@ rulespec_registry.register(
         group=RulespecGroupCheckParametersNetworking,
         item_spec=lambda: TextInput(title=_("Cisco ACI interface parameters")),
         match_type="dict",
-        parameter_valuespec=_parameter_valuespec_nc_coriant_psus,
+        parameter_valuespec=_parameter_valuespec_aci_l1_phys_if_levels,
         title=lambda: _("Cisco ACI interface parameters"),
+    )
+)
+
+
+def _parameter_valuespec_aci_health_levels():
+    return Dictionary(
+        help=_(
+            'To obtain the data required for this check, please configure'
+            ' the datasource program "Cisco ACI".'
+        ),
+        elements=[
+            (
+                'health_levels',
+                Tuple(
+                    title=_("Fabric Health Levels"),
+                    help=_(
+                        "An alert will be raised if the fabric health is lower than "
+                        "the configured level."
+                    ),
+                    elements=[
+                        Integer(
+                            title=_("Warning at"),
+                            minvalue=0,
+                            maxvalue=100,
+                            default_value=95,
+                        ),
+                        Integer(
+                            title=_("Critical at"),
+                            minvalue=0,
+                            maxvalue=100,
+                            default_value=85,
+                        ),
+                    ],
+                ),
+            ),
+        ],
+    )
+
+
+rulespec_registry.register(
+    CheckParameterRulespecWithoutItem(
+        check_group_name="aci_health_levels",
+        group=RulespecGroupCheckParametersNetworking,
+        match_type="dict",
+        parameter_valuespec=_parameter_valuespec_aci_health_levels,
+        title=lambda: _("Cisco ACI Fabric Health Levels"),
+    )
+)
+
+
+def _parameter_valuespec_aci_node_levels():
+    return Dictionary(
+        help=_(
+            'To obtain the data required for this check, please configure'
+            ' the datasource program "Cisco ACI".'
+        ),
+        elements=[
+            (
+                'health_levels',
+                Tuple(
+                    title=_("Node Health Levels"),
+                    help=_(
+                        "An alert will be raised if the node health is lower than "
+                        "the configured level."
+                    ),
+                    elements=[
+                        Integer(
+                            title=_("Warning at"),
+                            minvalue=0,
+                            maxvalue=100,
+                            default_value=95,
+                        ),
+                        Integer(
+                            title=_("Critical at"),
+                            minvalue=0,
+                            maxvalue=100,
+                            default_value=85,
+                        ),
+                    ],
+                ),
+            ),
+        ],
+    )
+
+
+rulespec_registry.register(
+    CheckParameterRulespecWithItem(
+        check_group_name="aci_node_levels",
+        group=RulespecGroupCheckParametersNetworking,
+        item_spec=lambda: TextInput(title=_("Cisco ACI Node Health Levels")),
+        match_type="dict",
+        parameter_valuespec=_parameter_valuespec_aci_node_levels,
+        title=lambda: _("Cisco ACI Node Health Levels"),
     )
 )
