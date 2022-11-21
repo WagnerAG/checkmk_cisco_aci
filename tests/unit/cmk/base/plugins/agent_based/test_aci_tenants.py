4c5cc1ebeb907d41f0594f4526b2a019689cc1ca
Roger Ellenberger <ellr@wagner.ch>
2022-11-21 19:00:33 +0100

    fix linting errors

--- tests/unit/cmk/base/plugins/agent_based/test_aci_tenants.py
+++ tests/unit/cmk/base/plugins/agent_based/test_aci_tenants.py
@@ -12,7 +12,7 @@
 # to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 # Boston, MA 02110-1301 USA.
 
-from typing import Tuple, List, Optional, Dict
+from typing import Tuple, List, Dict
 
 import pytest
 
