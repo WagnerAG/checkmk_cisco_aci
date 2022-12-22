a6bcf05e43d507127a9a929d2d91b9ce88e8e271
Roger Ellenberger <ellr@wagner.ch>
2022-12-22 16:01:14 +0100

    do not use python2 in build.sh

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAME=$(python -c 'print(eval(open("package").read())["name"])')
+NAME=$(python3 -c 'print(eval(open("package").read())["name"])')
 rm /omd/sites/cmk/var/check_mk/packages/* ||:
 ln -s $WORKSPACE/package /omd/sites/cmk/var/check_mk/packages/$NAME
 
