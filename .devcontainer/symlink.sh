642e49858b9374bd81dfae7499b159aef3d6300c
dampfhamm3r <roland.wyss@wagner.ch>
2024-06-12 06:05:09 +0000

    update to cmk 2.2.0; add ruff; remove flake8.

--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -8,19 +8,24 @@ done;
 rm -rfv $OMD_ROOT/local/lib/check_mk/notification_plugins
 ln -sv $WORKSPACE/notification_plugins $OMD_ROOT/local/lib/check_mk/notification_plugins
 
+mkdir -p $OMD_ROOT/local/lib/check_mk/gui/plugins/wato/
+mkdir -p $OMD_ROOT/local/lib/check_mk/gui/plugins/wato/check_parameters
+ln -sv $WORKSPACE/agent_based_check_parameters $OMD_ROOT/local/lib/check_mk/gui/plugins/wato/check_parameters
+
 rm -rfv $OMD_ROOT/local/lib/check_mk/special_agents
-ln -sv $WORKSPACE/special_agents $OMD_ROOT/local/lib/check_mk/special_agents
+ln -sv $WORKSPACE/agent_based_special $OMD_ROOT/local/lib/check_mk/special_agents
 
-rm -rfv $OMD_ROOT/local/lib/check_mk/base/plugins/agent_based
-ln -sv $WORKSPACE/agent_based $OMD_ROOT/local/lib/check_mk/base/plugins/agent_based
+rm -rfv $OMD_ROOT/local/lib/python3/cmk/base/plugins/agent_based
+ln -sv $WORKSPACE/agent_based $OMD_ROOT/local/lib/python3/cmk/base/plugins/agent_based
 
-rm -rfv $OMD_ROOT/local/lib/nagios/plugins
-ln -sv $WORKSPACE/nagios_plugins $OMD_ROOT/local/lib/nagios/plugins
-
+rm -rfv $OMD_ROOT/local/tmp
 ln -sv $WORKSPACE/temp $OMD_ROOT/local/tmp
 
 rm -rfv $OMD_ROOT/local/lib/check_mk/base/cee/plugins/bakery
-mkdir -p $OMD_ROOT/local/lib/check_mk/base/cee/plugins
-ln -sv $WORKSPACE/bakery $OMD_ROOT/local/lib/check_mk/base/cee/plugins/bakery
+mkdir -p $OMD_ROOT/local/lib/python3/cmk/base/cee/plugins
+ln -sv $WORKSPACE/bakery $OMD_ROOT/local/lib/python3/cmk/base/cee/plugins/bakery
 
-htpasswd -b $OMD_ROOT/etc/htpasswd cmkadmin cmkadmin
+source /omd/sites/cmk/.profile && echo 'cmkadmin' | /omd/sites/cmk/bin/cmk-passwd -i cmkadmin
+
+echo "▹ Starting OMD... "
+omd restart
