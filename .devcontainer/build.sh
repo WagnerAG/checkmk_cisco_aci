8193959f9d2305660d5aef08b5ac3924e48e3331
Simon Meister <simon.meister@wagner.ch>
2026-01-05 15:53:30 +0000

    update boilerplate

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+set -euo pipefail
+
 NAME=$(python3 -c 'print(eval(open("package").read())["name"])')
 VERSION=$(python3 -c 'print(eval(open("package").read())["version"])')
-rm /omd/sites/cmk/var/check_mk/packages/${NAME} \
-   /omd/sites/cmk/var/check_mk/packages_local/${NAME}-*.mkp ||:
+WORKSPACE=$(pwd)
+
+rm /omd/sites/cmk/var/check_mk/packages/* ||:
+ln -s $WORKSPACE/package /omd/sites/cmk/var/check_mk/packages/$NAME
+
 
 mkp -v package package 2>&1 | sed '/Installing$/Q' ||:
 
+rm $NAME-$VERSION.mkp ||:
 cp /omd/sites/cmk/var/check_mk/packages_local/$NAME-$VERSION.mkp .
 
 mkp inspect $NAME-$VERSION.mkp
+
+# Set Outputs for GitHub Workflow steps (using environment files)
+if [ -n "${GITHUB_OUTPUT:-}" ]; then
+    {
+        echo "pkgfile=$(ls *.mkp)"
+        echo "pkgname=${NAME}"
+        echo "pkgversion=${VERSION}"
+    } >> "$GITHUB_OUTPUT"
+fi
--- .devcontainer/setpwd.sh
+++ .devcontainer/setpwd.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-PKGNAME=$(python3 -c 'print(eval(open("package").read())["name"])')
-ln -sv $WORKSPACE $OMD_ROOT/local/lib/python3/cmk_addons/plugins/$PKGNAME
-
-rm -rfv $OMD_ROOT/local/lib/nagios/plugins
-ln -sv $WORKSPACE/nagios_plugins $OMD_ROOT/local/lib/nagios/plugins
-
-rm -rfv $OMD_ROOT/local/tmp
-ln -sv $WORKSPACE/temp $OMD_ROOT/local/tmp
-
 source /omd/sites/cmk/.profile && echo 'cmkadmin' | /omd/sites/cmk/bin/cmk-passwd -i cmkadmin
