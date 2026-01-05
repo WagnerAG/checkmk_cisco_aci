b604883215aec0964d667971180c8c2d78f335e3
Simon Meister <simon.meister@wagner.ch>
2026-01-05 23:12:01 +0000

    fix aci controller

--- cmk_addons/plugins/cisco_aci/agent_based/aci_controller.py
+++ cmk_addons/plugins/cisco_aci/agent_based/aci_controller.py
@@ -21,17 +21,10 @@ Authors:    Samuel Zehnder <zehnder@netcloud.ch>
 """
 
 from __future__ import annotations
+
 from typing import List, NamedTuple
 
-from cmk.agent_based.v2 import (
-    CheckResult,
-    DiscoveryResult,
-    AgentSection,
-    CheckPlugin,
-    Result,
-    Service,
-    State,
-)
+from cmk.agent_based.v2 import AgentSection, CheckPlugin, CheckResult, DiscoveryResult, Result, Service, State
 
 HEALTHY_CONTROLLER_STATUS: str = "in-service"
 
@@ -42,17 +35,21 @@ class ACIController(NamedTuple):
     status: str
     serial: str
     model: str
+    fault_crit: str
+    fault_maj: str
+    fault_minor: str
+    fault_warn: str
     descr: str
 
 
 def parse_aci_controller(string_table) -> List[ACIController]:
     """
     Exmple output:
-        controller 1 APIC1 in-service FCH1835V2FM APIC-SERVER-M1 APIC-SERVER-M1
+        controller 1 APIC1 in-service FCH1835V2FM APIC-SERVER-M4 0 -1 0 0 APIC-SERVER-M4
 
-        controller 1 ACI01 in-service FCH1935V1Z8 APIC-SERVER-M2 APIC-SERVER-M2
+        controller 1 ACI01 in-service FCH1935V1Z8 APIC-SERVER-M4 0 -1 0 0APIC-SERVER-M4
     """
-    return [ACIController(controller_id, name, status, serial, model, descr) for _, controller_id, name, status, serial, model, descr in string_table]
+    return [ACIController(controller_id, name, status, serial, model, fault_crit, fault_maj, fault_minor, fault_warn, descr) for _, controller_id, name, status, serial, model, fault_crit, fault_maj, fault_minor, fault_warn, descr in string_table]
 
 
 def discover_aci_controller(section: List[ACIController]) -> DiscoveryResult:
@@ -63,7 +60,48 @@ def discover_aci_controller(section: List[ACIController]) -> DiscoveryResult:
 def check_aci_controller(item: str, section: List[ACIController]) -> CheckResult:
     for ctrl in section:
         if item == ctrl.controller_id:
-            yield Result(state=State.OK if ctrl.status == HEALTHY_CONTROLLER_STATUS else State.CRIT, summary=f"{ctrl.name} is {ctrl.status}, Model: {ctrl.model}, Serial: {ctrl.serial}")
+            fault_crit = int(ctrl.fault_crit)
+            fault_maj = int(ctrl.fault_maj)
+            fault_minor = int(ctrl.fault_minor)
+            fault_warn = int(ctrl.fault_warn)
+
+            details = f"""
+                    Unacknowledged APIC Faults:
+                    - Crit: {fault_crit}
+                    - Maj: {fault_maj}
+                    - Minor: {fault_minor}
+                    - Warning: {fault_warn}
+                """
+
+            if fault_crit > 0 or fault_maj > 0 or ctrl.status != HEALTHY_CONTROLLER_STATUS:
+                faults = fault_maj + fault_crit
+                yield Result(
+                    state=State.CRIT,
+                    summary=f"{ctrl.name} is {ctrl.status}, Unacknowledged Faults: {faults}, Model: {ctrl.model}, Serial: {ctrl.serial}",
+                    details=details,
+                )
+                break
+            elif fault_minor > 0 or fault_warn > 0:
+                faults = str(fault_minor + fault_warn)
+                yield Result(
+                    state=State.WARN,
+                    summary=f"{ctrl.name} is {ctrl.status}, Unacknowledged Faults: {faults}, Model: {ctrl.model}, Serial: {ctrl.serial}",
+                    details=details,
+                )
+                break
+            elif fault_crit < 0 or fault_maj < 0 or fault_minor < 0 or fault_warn < 0:
+                yield Result(
+                    state=State.WARN,
+                    summary=f"{ctrl.name} is {ctrl.status}, Unacknowledged Faults: got negative number, Model: {ctrl.model}, Serial: {ctrl.serial}",
+                    details=f'{details}\nThe difference between “faults - faultsAcknowledged” results in a negative number for one of the error categories crit/maj/minor/warn.\nThis means that there are probably "stale faults" on the APIC, which are output via the API but are not visible in the GUI.\nPlease investigate and correct the errors.',
+                )
+                break
+            else:
+                faults = fault_maj + fault_crit + fault_minor + fault_warn
+                yield Result(
+                    state=State.OK,
+                    summary=f"{ctrl.name} is {ctrl.status}, Unacknowledged Faults: {faults}, Model: {ctrl.model}, Serial: {ctrl.serial}",
+                )
             break
     else:
         yield Result(state=State.UNKNOWN, summary="Sorry - item not found")
